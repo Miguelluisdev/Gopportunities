bde81eacce64442e72987515f31b932de4660bfd
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-07-18 16:12:02 -0300

    feat: add routes for api

--- router/router.go
+++ router/router.go
@@ -3,11 +3,9 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	//  Initialize router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	 initializeRoutes(router)
+	
 	router.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
