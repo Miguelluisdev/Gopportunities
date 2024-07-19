e47e4837d4ff566fa2ba095b621c4df03c6f48d7
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-07-18 22:31:54 -0300

    feat: add openning schemas and create a logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/Miguelluiddev/Gopportunities/router"
+import (
+	"github.com/Miguelluiddev/Gopportunities/config"
+	"github.com/Miguelluiddev/Gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err)
+
+	}
+
 	router.Initialize()
 }
